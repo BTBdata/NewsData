6450f79b0a06942e6f952e84fb6ea05135ab5f32
BTBdata <142769830+BTBdata@users.noreply.github.com>
2023-11-08 22:23:36 -0500

    Create dev_prep_news_for_sql.py
    dev (local) prep raw news folders from server prior to pushing to BTB database.

---
+++ dev_prep_news_for_sql.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 23:21:37 2023
+
+@author: BTB Data Solutions - Benjamin Bergenstein:
+    script to fetch raw news articles and standardize them before pushing to database.
+"""
+
+import pandas as pd
+import os
+import datetime
+import time
+
+def prep_main_news():
+    # dir of files to process
+    directory = r'C:\Users\benja\OneDrive\Documents\BTBdataSolutions_Project\BTBdataSolutions\main\raw_text'    
+    # iterate over files in directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        timestr = time.strftime("%Y%m%d-%H%M%S")        
+        file_name = timestr + '.csv'  
+        df = pd.read_csv(f,encoding='utf-8')   
+    
+        df.rename(columns={'text':'article_text', 'source':'source_type'}, inplace=True)
+        df2 = df[['title_id', 'title','article_text','source_type','parsed_date']]
+        df2.to_csv(Fr'C:\Users\benja\OneDrive\Documents\BTBdataSolutions_Project\BTBdataSolutions\main\sql_setup\push\{filename}', index=False)
+ 
+def prep_bbc_news():
+    # dir of files to process
+    directory = r'C:\Users\benja\OneDrive\Documents\BTBdataSolutions_Project\BTBdataSolutions\main\sql_setup\raw_bbc'    
+    # iterate over files in directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        timestr = time.strftime("%Y%m%d-%H%M%S")        
+        file_name = timestr + '.csv'  
+        df = pd.read_csv(f,encoding='utf-8')     
+        
+        df.rename(columns={'title_hash':'title_id', 'summary':'article_text','type':'source_type'}, inplace=True)                         
+        db = df[['title_id', 'title','article_text','source_type','parsed_date']]       
+        db.to_csv(Fr'C:\Users\benja\OneDrive\Documents\BTBdataSolutions_Project\BTBdataSolutions\main\sql_setup\push\{file_name}', index=False)
+        
+def main():
+    #prep_main_news()
+    prep_bbc_news()  
+
+
+      
+if __name__ == "__main__":
+    main()
